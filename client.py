d76bde14954665a32af6cf2c1eb48031e0060be2
Anthony Wu <anthonycwu@yahoo.com>
2023-03-19 15:58:45 -0400

    Fixed client.py

--- client.py
+++ client.py
@@ -3,3 +3,24 @@
 # Student Name: Anthony Wu
 # Student ID: wuant
 
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("Connected to %s port %s" % (HOST, PORT))
+
+    print("Type /q to quit; enter a message to send...\n")
+    while True:
+        print('Client> ', end='', flush=True)
+
+        s.sendall(str.encode(input()))
+
+        data = s.recv(1024)
+
+        if (not data) or data.decode()=='/q':
+            break
+        else:
+            print("Server: " + data.decode())
